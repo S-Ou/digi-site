469dce1b1b70103de704a3114425617a02c5dd0e
SOu <s.j.ou@student.saintkentigern.com>
2018-08-24 14:53:56 +1200

    Final submission

--- assessment/scipt.js
+++ assessment/scipt.js
@@ -1,15 +1,15 @@
-(function ($) {    
-    $(document).ready(function () {        
+(function ($) {
+    $(document).ready(function () {
         $(".toggle-nav").on("click", function () {
             toggleNavigation($(this), $(".nav-pane"));
             changeLetters($(this));
-        }); 
-        
+        });
+
         $("#mainbody").on("click", function () {
             togglebody($(".toggle-nav"), $(".nav-pane"));
             changeLetters($(".toggle-nav"));
-        }); 
-        
+        });
+
         function togglebody(btn, nav) {
             btn.toggleClass("open");
             nav.toggleClass("open");
@@ -38,31 +38,31 @@
                 u.text("U");
             }
         }
-        
-        
+
+
         function captions() {
-            $("img").each(function(i){
+            $("img").each(function (i) {
                 var imgWidth = $('img').eq(i).css('width');
                 $('.imgtxt').eq(i).css('width', imgWidth);
             });
 
-            $("img").each(function(i){
+            $("img").each(function (i) {
                 var imgHeight = $('img').eq(i).css('height');
                 $('.imgtxt').eq(i).css('height', imgHeight);
-//                console.log(imgHeight);
-//                console.log(i);
+                //                console.log(imgHeight);
+                //                console.log(i);
             });
 
-            $("img").each(function(i){
+            $("img").each(function (i) {
                 var imgHeight = $('img').eq(i).css('height');
                 imgHeight = parseInt(imgHeight.slice(0, -2)) + 30;
                 imgHeight = imgHeight + "px";
-                $('.imgtxt').eq(i).css('transform', 'translateY(-'+imgHeight+')');
+                $('.imgtxt').eq(i).css('transform', 'translateY(-' + imgHeight + ')');
             });
         }
-        
+
         captions()
-       
-//        $(window).resize(captions())
+
+        //        $(window).resize(captions())
     });
 })(jQuery);
